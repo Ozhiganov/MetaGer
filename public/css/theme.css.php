09d8baacac5b2d04478c8896379a0f0c48db0888
Karl Hasselbring <Karl Hasselbring>
2017-10-10 10:34:11 +0200

    Added new Search bar WIP

--- public/css/theme.css.php
+++ public/css/theme.css.php
@@ -10,14 +10,6 @@ if (isset($_GET['r']) && isset($_GET['g']) && isset($_GET['b']) && isset($_GET['
 
 @CHARSET "UTF-8";
 
-input[type=text]:focus, textarea:focus, input[type=email]:focus, input[type=tel]:focus {
-	outline-color: <?=$color?>;
-	-webkit-box-shadow: 0px 0px 2px 2px <?=$colorRGBA?>;
-	-moz-box-shadow: 0px 0px 2px 2px <?=$colorRGBA?>;
-	box-shadow: 0px 0px 2px 2px <?=$colorRGBA?>;
-    border-color: <?=$colorRGBA?>;
-}
-
 #mglogo > a {
 	background-image: linear-gradient(<?=$color?> 0%, <?=$color?> 250%);
     background-color: transparent;
