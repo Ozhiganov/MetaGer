fc9ef18f1363e9af019a46313e5d665ce1943c9b
Karl Hasselbring <Karl Hasselbring>
2017-10-10 11:17:51 +0200

    Redesigned Research Bar

--- resources/views/layouts/researchandtabs.blade.php
+++ resources/views/layouts/researchandtabs.blade.php
@@ -12,23 +12,19 @@
 	<header id="research" class="row">
 		<nav class="navbar navbar-default navbar-fixed-top navbar-resultpage">
 			<div class="container">
-				<div class="row">
-					<div class="col-xs-3 logo dense-col">
-						<a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/") }}"><h1>M<span class="hidden-xs">eta</span>G<span class="hidden-xs">er</span></h1></a>
-					</div>
-					<div class="col-xs-9 dense-col">
-						<form method="{{ Request::method() }}" accept-charset="UTF-8" class="form search-bar-input" id="submitForm">
-							<div class="searchbar">
-								<input autocomplete="off" class="form-control" form="submitForm" id="eingabeTop" name="eingabe" placeholder="Suchbegriffe erweitern/verändern, oder völlig neue Suche:" tabindex="1" type="text" value="{{ $eingabe }}" required />
-								<button type='submit' form="submitForm" id='search'><i class="fa fa-search" aria-hidden="true"></i></button>
-							</div>
-							@foreach( $metager->request->all() as $key => $value)
-								@if($key !== "eingabe" && $key !== "page" && $key !== "next")
-									<input type='hidden' name='{{ $key }}' value='{{ $value }}' form='submitForm' />
-								@endif
-							@endforeach
-						</form>
-					</div>
+				<div class="row" id="logo-searchbar-container">
+					<a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/") }}"><h1 class="mg-logo">M<span class="hidden-xs">eta</span>G<span class="hidden-xs">er</span></h1></a>
+					<form method="{{ Request::method() }}" accept-charset="UTF-8" class="form search-bar-input" id="submitForm">
+						<div class="searchbar">
+							<input autocomplete="off" class="form-control" form="submitForm" id="eingabeTop" name="eingabe" placeholder="Suchbegriffe erweitern/verändern, oder völlig neue Suche:" tabindex="1" type="text" value="{{ $eingabe }}" required />
+							<button type='submit' form="submitForm" id='search'><i class="fa fa-search" aria-hidden="true"></i></button>
+						</div>
+						@foreach( $metager->request->all() as $key => $value)
+							@if($key !== "eingabe" && $key !== "page" && $key !== "next")
+								<input type='hidden' name='{{ $key }}' value='{{ $value }}' form='submitForm' />
+							@endif
+						@endforeach
+					</form>
 				</div>
 			</div>
 		</nav>
