e9625e472e98a5582ed6c0b02ad12223914b91c3
Karl Hasselbring <Karl Hasselbring>
2017-11-07 09:23:36 +0100

    Fehler auf der Startseite behoben

---
+++ resources/views/modals/create-focus-modal.blade.php
@@ -0,0 +1,73 @@
+<div id="create-focus-modal" class="modal fade" tab-index="-1" role="dialog">
+	<div class="modal-dialog modal-lg">
+		<div class="content modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4>
+					@lang("index.focus-creator.head")
+				</h4>
+				<p class="text-muted">@lang("index.focus-creator.description")</p>
+			</div>
+			<div class="modal-body">
+				<div class="clearfix">
+					<div class="pull-left">
+						<label for="focus-name">@lang('index.focus-creator.focusname')</label>
+						<input id="focus-name" type="text" name="focus-name" placeholder="@lang('index.focus-creator.name-placeholder')">
+						<input id="original-id" type="hidden" name="original-id" value="">
+					</div>
+					<div class="settings-modal-buttons pull-right">
+						<button class="delete-focus-btn btn btn-danger">
+							@lang('index.focus-creator.delete')
+						</button>
+						<button class="save-focus-btn btn btn-primary">
+							@lang('index.focus-creator.save')
+						</button>
+					</div>
+				</div>
+				{{--
+				<h2>{!! trans('settings.suchmaschinen.1') !!}
+					<small>
+						<button type="button" class="btn btn-link allUnchecker hide">{!! trans('settings.suchmaschinen.2') !!}</button>
+					</small>
+				</h2>
+				--}} @foreach( $foki as $fokus => $sumas )
+				<div class="headingGroup {{ $fokus }}">
+					<h3 class="focus-category">
+						@lang("settings.foki." . $fokus) {{--
+						<small>
+							<button type="button" class="checker btn btn-link hide" data-type="{{ $fokus }}">{!! trans('settings.suchmaschinen.3') !!}</button>
+						</small>
+						--}}
+					</h3>
+					<div class="row">
+						@foreach( $sumas as $name => $data )
+						<div class="col-sm-6 col-md-4 col-lg-3">
+							<div class="checkbox settings-checkbox">
+								<label>
+									<input type="checkbox" name="engine_{{ $name }}" class="focusCheckbox" @if ($fokus=='web' ) checked @endif>{{ $data['displayName'] }}
+									<a class="settings-icon" target="_blank" rel="noopener" href="{{ $data['url'] }}">
+										<i class="fa fa-link" aria-hidden="true"></i>
+									</a>
+								</label>
+							</div>
+						</div>
+						@endforeach
+					</div>
+				</div>
+				@endforeach
+				<div class="clearfix">
+					<div class="settings-modal-buttons pull-right">
+						<button class="delete-focus-btn btn btn-danger">
+							@lang('index.focus-creator.delete')
+						</button>
+						<button class="save-focus-btn btn btn-primary">
+							@lang('index.focus-creator.save')
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
