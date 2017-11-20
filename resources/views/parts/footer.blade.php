01e2e3297cc8e8709d03769f24982eabaf591ec2
Karl Hasselbring <Karl Hasselbring>
2017-11-20 08:30:39 +0100

    Improved: Startseite hat jetzt eine mobile Version

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -5,11 +5,11 @@
     <img src="/img/suma_ev_logo-m1-greyscale.png" alt="SUMA-EV Logo"></a>
   </div>
   <div id="info">
-    <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "kontakt") }}">{{ trans('staticPages.nav5') }}</a> - <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "impressum") }}">{{ trans('staticPages.nav8') }}</a>
-    {{ trans('staticPages.sumaev.1') }}<a href="https://www.suma-ev.de/">{{ trans('staticPages.sumaev.2') }}</a>
+    <span><a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "kontakt") }}">{{ trans('staticPages.nav5') }}</a> - <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "impressum") }}">{{ trans('staticPages.nav8') }}</a></span>
+    <span class="hidden-xs">{{ trans('staticPages.sumaev.1') }}<a href="https://www.suma-ev.de/">{{ trans('staticPages.sumaev.2') }}</a></span>
   </div>
   <div>
-    <a href="https://www.uni-hannover.de/"  >
+    <a href="https://www.uni-hannover.de/">
     <img src="/img/luh_metager.png" alt="LUH Logo"></a>
   </div>
 </footer>
