31aad2b4230fc05ba776c159a9a13a2ae4933e94
Karl Hasselbring <Karl Hasselbring>
2017-11-20 10:25:02 +0100

    Refactoring: Styles zusammengefasst und relikte gesammelt

--- gulpfile.js
+++ gulpfile.js
@@ -11,19 +11,18 @@ require('laravel-elixir-vue-2')
  |
  */
 elixir(function (mix) {
+  // less
   mix.less('default.less', 'public/css/themes/default.css')
-  /*
-   lib JS files not mixed (specific pages only)
-  */
+  mix.less('metager/beitritt.less', 'public/css/beitritt.css')
+  // js
   mix.scripts(['lib/jquery.js', 'lib/jquery-ui.min.js', 'lib/bootstrap.js', 'lib/lightslider.js', 'lib/masonry.js', 'lib/imagesloaded.js', 'lib/openpgp.min.js', 'lib/iframeResizer.min.js', 'lib/md5.js'], 'public/js/lib.js')
   mix.scripts(['scriptStartPage.js', 'results.js'], 'public/js/scriptStartPage.js');
   mix.scripts(['scriptResultPage.js', 'results.js'], 'public/js/scriptResultPage.js');
   mix.scripts(['searchbar.js'], 'public/js/searchbar.js');
-  //mix.scripts(['scriptSubpages.js'], 'public/js/scriptSubpages.js');
-  mix.less('metager/beitritt.less', 'public/css/beitritt.css')
   // utility
   mix.scripts(['utility.js'], 'public/js/utility.js');
   mix.less('utility.less', 'public/css/utility.css');
+  // Versioning
   mix.version(['css/themes/default.css', 'js/lib.js', 'js/utility.js', 'css/utility.css', 'js/widgets.js', 'js/editLanguage.js', 'js/kontakt.js', 'js/scriptResultPage.js', 'js/scriptStartPage.js', 'js/settings.js', 'css/beitritt.css']);
 
 })
