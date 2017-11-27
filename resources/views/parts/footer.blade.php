ffbf334519dc6c47ccee0ba3827a1e192a0cf6e7
Karl Hasselbring <Karl Hasselbring>
2017-11-27 08:50:01 +0100

    Improved: Alle Seiten haben jetzt ein flüssiges und modernes Design

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,16 +1,18 @@
 @if ($type === 'startpage' || $type === 'subpage')
 <footer class="{{ $id }} noprint">
-  <div>
+  <div class="imagebox">
     <a href="https://www.suma-ev.de/"  >
-    <img src="/img/suma_ev_logo-m1-greyscale.png" alt="SUMA-EV Logo"></a>
+      <img src="/img/suma_ev_logo-m1-greyscale.png" alt="SUMA-EV Logo">
+    </a>
   </div>
   <div id="info">
     <span><a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "kontakt") }}">{{ trans('staticPages.nav5') }}</a> - <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "impressum") }}">{{ trans('staticPages.nav8') }}</a></span>
     <span class="hidden-xs">{{ trans('staticPages.sumaev.1') }}<a href="https://www.suma-ev.de/">{{ trans('staticPages.sumaev.2') }}</a></span>
   </div>
-  <div>
+  <div class="imagebox">
     <a href="https://www.uni-hannover.de/">
-    <img src="/img/luh_metager.png" alt="LUH Logo"></a>
+      <img src="/img/luh_metager.png" alt="LUH Logo">
+    </a>
   </div>
 </footer>
 @elseif ($type === 'resultpage')
