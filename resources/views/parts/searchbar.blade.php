979628b0076e8d8bec02efb98f46ef86bdf37bed
Aria <aria@suma-ev.de>
2017-11-27 10:48:56 +0100

    UI verbessert

--- resources/views/parts/searchbar.blade.php
+++ resources/views/parts/searchbar.blade.php
@@ -8,6 +8,8 @@
 					<i class="fa fa-chevron-down"></i>
 				</button>
 				<div class="search-option-frame" style="display:none;">
+					<div class="searchbar-options-arrow">
+					</div>
 					<select id="focus-select" name="focus" style="font-family: FontAwesome, sans-serif;">
 						<option value="web" style="font-family: FontAwesome, sans-serif;" selected>&#xf0ac; Websuche</option>
 						<option value="nachrichten" style="font-family: FontAwesome, sans-serif;">&#xf0a1; Nachrichtensuche</option>
