7a514254afdfd74cdaf1687b740f70c9fa839117
Karl Hasselbring <Karl Hasselbring>
2017-12-05 11:05:25 +0100

    Refactoring: Tidyed and commented the result-saver.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,8 @@ elixir(function (mix) {
   mix.less('metager/beitritt.less', 'public/css/beitritt.css')
   // js
   mix.scripts(['lib/jquery.js', 'lib/jquery-ui.min.js', 'lib/bootstrap.js', 'lib/lightslider.js', 'lib/masonry.js', 'lib/imagesloaded.js', 'lib/openpgp.min.js', 'lib/iframeResizer.min.js', 'lib/md5.js'], 'public/js/lib.js')
-  mix.scripts(['scriptStartPage.js', 'results.js'], 'public/js/scriptStartPage.js');
-  mix.scripts(['scriptResultPage.js', 'results.js', 'quicktips.js'], 'public/js/scriptResultPage.js');
+  mix.scripts(['scriptStartPage.js', 'result-saver.js'], 'public/js/scriptStartPage.js');
+  mix.scripts(['scriptResultPage.js', 'result-saver.js', 'quicktips.js'], 'public/js/scriptResultPage.js');
   mix.scripts(['searchbar.js', 'focus-creator.js'], 'public/js/searchbar.js');
   // utility
   mix.scripts(['utility.js'], 'public/js/utility.js');
