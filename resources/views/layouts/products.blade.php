a6b839499e59f59af99e21379b5cf738d543d29a
Karl Hasselbring <Karl Hasselbring>
2017-12-05 08:27:04 +0100

    Refactoring: Produkte an neues Design angepasst, noch ncht repariert

--- resources/views/layouts/products.blade.php
+++ resources/views/layouts/products.blade.php
@@ -1,21 +1,20 @@
-<div class="row lightSliderContainer hidden">
-	<div class="col-sm-1 hidden-xs"></div>
-	<div class="result-information col-xs-12 col-sm-11">
-		<span id="mark">Produktanzeigen:</span>
-		<ul id="products" class="lightSlider" >
-			@foreach($products as $product)
-				<li>
-					<div class="product">
-						<a href="{{$product["link"]}}" title="{{$product["titel"]}}" target="_blank" rel="noopener">
-							<div class="price">{!!$product["price"]!!}</div>
-							<img src="{{ $metager->getImageProxyLink($product["image"]) }}" />
-							<p class="title">{{$product["titel"]}}</p>
-							<p class="shop">{{$product["gefVon"]}}</p>
-							<p class="shipping">{{$product["additionalInformation"]["shipping"]}}</p>
-						</a>
-					</div>
-				</li>
-			@endforeach
-		</ul>
-	</div>
+<div class="result product">
+	<h2 class="result-title">
+		<span id="mark">@lang('produktanzeigen')</span>
+	</h2>
+	<ul id="products" class="lightSlider" >
+		@foreach($products as $product)
+			<li>
+				<div class="product">
+					<a href="{{$product["link"]}}" title="{{$product["titel"]}}" target="_blank" rel="noopener">
+						<div class="price">{!!$product["price"]!!}</div>
+						<img src="{{ $metager->getImageProxyLink($product["image"]) }}" />
+						<p class="title">{{$product["titel"]}}</p>
+						<p class="shop">{{$product["gefVon"]}}</p>
+						<p class="shipping">{{$product["additionalInformation"]["shipping"]}}</p>
+					</a>
+				</div>
+			</li>
+		@endforeach
+	</ul>
 </div>
