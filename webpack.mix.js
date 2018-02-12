73dfdfec29258bf2b355d6eb52ed492a5b5d734b
Karl Hasselbring <Karl Hasselbring>
2018-02-12 11:52:45 +0100

    Das Javascript für den Foku-Ersteller wirft jetzt keine Fehler mehr

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,11 +15,12 @@ mix.less('resources/assets/less/default.less', 'public/css/themes/default.css')
   .less('resources/assets/less/metager/beitritt.less', 'public/css/beitritt.css')
   .less('resources/assets/less/utility.less', 'public/css/utility.css')
   // js
-  .scripts(['resources/assets/js/lib/jquery.js', 'resources/assets/js/lib/jquery-ui.min.js', 'resources/assets/js/lib/bootstrap.js', 'resources/assets/js/lib/lightslider.js', 
-  	'resources/assets/js/lib/masonry.js', 'resources/assets/js/lib/imagesloaded.js', 'resources/assets/js/lib/openpgp.min.js', 'resources/assets/js/lib/iframeResizer.min.js', 
-  	'resources/assets/js/lib/md5.js'], 'public/js/lib.js')
+  .scripts(['resources/assets/js/lib/jquery.js', 'resources/assets/js/lib/jquery-ui.min.js', 'resources/assets/js/lib/bootstrap.js', 'resources/assets/js/lib/lightslider.js',
+    'resources/assets/js/lib/masonry.js', 'resources/assets/js/lib/imagesloaded.js', 'resources/assets/js/lib/openpgp.min.js', 'resources/assets/js/lib/iframeResizer.min.js',
+    'resources/assets/js/lib/md5.js'], 'public/js/lib.js')
   .scripts(['resources/assets/js/scriptStartPage.js', 'resources/assets/js/result-saver.js'], 'public/js/scriptStartPage.js')
   .scripts(['resources/assets/js/scriptResultPage.js', 'resources/assets/js/result-saver.js'], 'public/js/scriptResultPage.js')
-  .scripts(['resources/assets/js/searchbar.js', 'resources/assets/js/focus-creator.js'], 'public/js/searchbar.js')
+  .scripts(['resources/assets/js/searchbar.js'], 'public/js/searchbar.js')
+  .scripts(['resources/assets/js/focus-creator.js'], 'public/js/focus-creator.js')
   // utility
   .scripts(['resources/assets/js/utility.js'], 'public/js/utility.js').version();
