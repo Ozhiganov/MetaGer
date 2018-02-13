7a319a7fcc8f0c4a06a208a1ea3f4cdf893d5436
Aria <aria@suma-ev.de>
2018-02-13 12:11:52 +0100

    Modal angepasst, erste Funktionalitaetsaenderungen

--- resources/views/modals/create-focus-modal.blade.php
+++ resources/views/modals/create-focus-modal.blade.php
@@ -10,53 +10,37 @@
 				<p class="text-muted">@lang("index.focus-creator.description")</p>
 			</div>
 			<div class="modal-body">
-				<div class="clearfix">
-					<div class="pull-left">
-						<label for="focus-name">@lang('index.focus-creator.focusname')</label>
-						<input id="focus-name" type="text" name="focus-name" placeholder="@lang('index.focus-creator.name-placeholder')">
-						<input id="original-id" type="hidden" name="original-id" value="">
-					</div>
-					<div class="settings-modal-buttons pull-right">
-						<button class="delete-focus-btn btn btn-danger">
-							@lang('index.focus-creator.delete')
-						</button>
-						<button class="save-focus-btn btn btn-primary">
-							@lang('index.focus-creator.save')
-						</button>
-					</div>
-				</div>
-				@foreach( App\Http\Controllers\FokiLoader::loadFoki() as $fokus => $sumas )
-					<div class="headingGroup {{ $fokus }}">
-						<input type="checkbox" id="toggle-{{ $fokus }}-dropdown" class="focus-dropdown-toggle">
-						<label for="toggle-{{ $fokus }}-dropdown" class="focus-category focus-dropdown-label h3">
-							@lang("settings.foki." . $fokus)
-						</label>
-						<div class="row">
-							@foreach( $sumas as $name => $data )
-							<div class="col-sm-6 col-md-4 col-lg-3">
-								<div class="checkbox settings-checkbox">
-									<label>
-										<input type="checkbox" name="engine_{{ strtolower($name) }}" class="focusCheckbox" @if ($fokus=='web' ) checked @endif>{{ $data['displayName'] }}
-										<a class="settings-icon" target="_blank" rel="noopener" href="{{ $data['url'] }}">
-											<i class="fa fa-link" aria-hidden="true"></i>
-										</a>
-									</label>
+				<form id="customize-search" role="form" method="POST"> 
+					@foreach( App\Http\Controllers\FokiLoader::loadFoki() as $fokus => $sumas )
+						<div class="headingGroup {{ $fokus }}">
+							<input type="checkbox" id="toggle-{{ $fokus }}-dropdown" class="focus-dropdown-toggle">
+							<label for="toggle-{{ $fokus }}-dropdown" class="focus-category focus-dropdown-label h3">
+								@lang("settings.foki." . $fokus)
+							</label>
+							<div class="row">
+								@foreach( $sumas as $name => $data )
+								<div class="col-sm-6 col-md-4 col-lg-3">
+									<div class="checkbox settings-checkbox">
+										<label>
+											<input type="checkbox" name="engine_{{ strtolower($name) }}" class="focusCheckbox" @if ($fokus=='web' ) checked @endif>{{ $data['displayName'] }}
+											<a class="settings-icon" target="_blank" rel="noopener" href="{{ $data['url'] }}">
+												<i class="fa fa-link" aria-hidden="true"></i>
+											</a>
+										</label>
+									</div>
 								</div>
+								@endforeach
 							</div>
-							@endforeach
+						</div>
+					@endforeach
+					<div class="clearfix">
+						<div class="settings-modal-buttons pull-right">
+							<button type="submit" class="save-focus-btn btn btn-primary">
+								@lang('index.focus-creator.save')
+							</button>
 						</div>
 					</div>
-				@endforeach
-				<div class="clearfix">
-					<div class="settings-modal-buttons pull-right">
-						<button class="delete-focus-btn btn btn-danger">
-							@lang('index.focus-creator.delete')
-						</button>
-						<button class="save-focus-btn btn btn-primary">
-							@lang('index.focus-creator.save')
-						</button>
-					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	</div>
