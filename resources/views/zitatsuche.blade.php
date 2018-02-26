7f5b28c35049b534203b4607eccdc5d105a296f4
Karl Hasselbring <Karl Hasselbring>
2018-02-26 11:45:09 +0100

    Übersetzungen hinzugefügt und Gespeicherte Ergebnisse repariert

--- resources/views/zitatsuche.blade.php
+++ resources/views/zitatsuche.blade.php
@@ -3,11 +3,11 @@
 @section('title', $title )
 
 @section('content')
-	<h1>{{ trans('zitatsuche.head.1') }}</h1>
-	<p>{{ trans('zitatsuche.p.1') }}</p>
+	<h1>{{ trans('zitatsuche.title') }}</h1>
+	<p>{{ trans('zitatsuche.subtitle') }}</p>
 	<form id="searchForm" class="form-inline" accept-charset="UTF-8">
 		<div class="form-group">
-			<label class="sr-only" for="q">Suchworte eingeben</label>
+			<label class="sr-only" for="q">{{ trans('zitatsuche.search-label') }}</label>
 			<div class="input-group">
 				<input type="text" class="form-control" id="q" name="q" placeholder="Suchworte" value="{{ $q }}">
 				<div class="input-group-addon"><button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button></div>
@@ -16,7 +16,7 @@
 	</form>
 	@if($q !== "")
 	<hr />
-	<h3>Ergebnisse für die Suche "{{$q}}":</h3>
+	<h3>{{ trans('zitatsuche.results-label') }} "{{$q}}":</h3>
 	@foreach($results as $author => $quotes)
 	<b><a href="{{ action('MetaGerSearch@search', ['eingabe' => $author, 'focus' => 'web', 'encoding' => 'utf8', 'lang' => 'all']) }}" target="_blank">{{$author}}</a>:</b>
 	<ul class="list-unstyled">
