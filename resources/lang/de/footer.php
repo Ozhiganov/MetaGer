62db5206892b1d626b0150230b5d28844520e990
Karl Hasselbring <Karl Hasselbring>
2018-02-27 11:44:19 +0100

    Übersetzungen für siebar übertragen

--- resources/lang/de/footer.php
+++ resources/lang/de/footer.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'sumaev.1'            => 'MetaGer wird entwickelt und betrieben vom ',
-    'sumaev.2'            => 'SUMA-EV - Verein für freien Wissenszugang.'
+    'sumaev.1' => 'MetaGer wird entwickelt und betrieben vom ',
+    'sumaev.2' => 'SUMA-EV - Verein für freien Wissenszugang.',
 ];
