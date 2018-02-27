f5dd4d92816e8a5da69785314ad7e0a0f733c56e
Karl Hasselbring <Karl Hasselbring>
2018-02-27 09:08:20 +0100

    Zusätzliche Kartendesigns eingefügt/verwendet

--- resources/views/widget/widget.blade.php
+++ resources/views/widget/widget.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 	<h1 class="page-title">{{ trans('widget.head') }}</h1>
-	<div class="card-heavy">
+	<div class="card-medium">
 		<p>{{ trans('widget.body.1') }}</p>
 		<p id="widgetLinks" class="btn-group"><a class="btn btn-default mutelink" href="websearch/">{{ trans('widget.body.2') }}</a><a class="btn btn-default mutelink" href="sitesearch/">{{ trans('widget.body.3') }}</a></p>
 		<p>{{ trans('widget.body.4') }}</p>
--- resources/views/zitatsuche.blade.php
+++ resources/views/zitatsuche.blade.php
@@ -3,27 +3,29 @@
 @section('title', $title )
 
 @section('content')
-	<h1>{{ trans('zitatsuche.head.1') }}</h1>
-	<p>{{ trans('zitatsuche.p.1') }}</p>
-	<form id="searchForm" class="form-inline" accept-charset="UTF-8">
-		<div class="form-group">
-			<label class="sr-only" for="q">Suchworte eingeben</label>
-			<div class="input-group">
-				<input type="text" class="form-control" id="q" name="q" placeholder="Suchworte" value="{{ $q }}">
-				<div class="input-group-addon"><button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button></div>
+	<h1 class="page-title">{{ trans('zitatsuche.head.1') }}</h1>
+	<div class="card-light">
+		<p>{{ trans('zitatsuche.p.1') }}</p>
+		<form id="searchForm" class="form-inline" accept-charset="UTF-8">
+			<div class="form-group">
+				<label class="sr-only" for="q">Suchworte eingeben</label>
+				<div class="input-group">
+					<input type="text" class="form-control" id="q" name="q" placeholder="Suchworte" value="{{ $q }}">
+					<div class="input-group-addon"><button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button></div>
+				</div>
 			</div>
-		</div>
-	</form>
-	@if($q !== "")
-	<hr />
-	<h3>Ergebnisse für die Suche "{{$q}}":</h3>
-	@foreach($results as $author => $quotes)
-	<b><a href="{{ action('MetaGerSearch@search', ['eingabe' => $author, 'focus' => 'web', 'encoding' => 'utf8', 'lang' => 'all']) }}" target="_blank">{{$author}}</a>:</b>
-	<ul class="list-unstyled">
-		@foreach($quotes as $quote)
-		<li><quote>"{{ $quote }}"</quote></li>
+		</form>
+		@if($q !== "")
+		<hr />
+		<h3>Ergebnisse für die Suche "{{$q}}":</h3>
+		@foreach($results as $author => $quotes)
+		<b><a href="{{ action('MetaGerSearch@search', ['eingabe' => $author, 'focus' => 'web', 'encoding' => 'utf8', 'lang' => 'all']) }}" target="_blank">{{$author}}</a>:</b>
+		<ul class="list-unstyled">
+			@foreach($quotes as $quote)
+			<li><quote>"{{ $quote }}"</quote></li>
+			@endforeach
+		</ul>
 		@endforeach
-	</ul>
-	@endforeach
-	@endif
+		@endif
+	</div>
 @endsection
