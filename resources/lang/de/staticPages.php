6f5d8ca33403570725219a4ffe45f60deb740c7f
Karl Hasselbring <Karl Hasselbring>
2018-02-27 11:44:19 +0100

    Übersetzungen für siebar übertragen

--- resources/lang/de/staticPages.php
+++ resources/lang/de/staticPages.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'opensearch'          => 'MetaGer: Sicher suchen & finden, Privatsphäre schützen',
-    
-    'meta.Description'    => 'Sicher suchen und finden unter Wahrung der Privatsphäre. Das digitale Wissen der Welt muss ohne Bevormundung durch Staaten oder Konzerne frei zugänglich sein und bleiben.',
-    'meta.Keywords'       => 'Internetsuche, privatsphäre, privacy, Suchmaschine, Datenschutz, Anonproxy, anonym suchen, Bildersuche, Suchmaschine, anonym, MetaGer, metager, metager.de',
-    'meta.language'       => 'de',
+    'opensearch' => 'MetaGer: Sicher suchen & finden, Privatsphäre schützen',
+
+    'meta.Description' => 'Sicher suchen und finden unter Wahrung der Privatsphäre. Das digitale Wissen der Welt muss ohne Bevormundung durch Staaten oder Konzerne frei zugänglich sein und bleiben.',
+    'meta.Keywords' => 'Internetsuche, privatsphäre, privacy, Suchmaschine, Datenschutz, Anonproxy, anonym suchen, Bildersuche, Suchmaschine, anonym, MetaGer, metager, metager.de',
+    'meta.language' => 'de',
 ];
