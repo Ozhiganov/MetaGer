4029df8f7bc964cc211a8f12912969209e8c5027
Karl Hasselbring <Karl Hasselbring>
2018-03-05 09:59:36 +0100

    Die Blade Dateien der Ergebnisausgabe haben jetzt eine kürzere benennung Einige Dinge wurden Zusammengefasst und Redundanzen entfernt

---
+++ resources/views/resultpages/results.blade.php
@@ -0,0 +1,28 @@
+@include('parts.errors')
+@include('parts.warnings')
+<div id="results">
+	@if($mobile)
+		@include('layouts.ad', ['ad' => $metager->popAd()])
+	@else
+		@for($i = 0; $i <= 2; $i++)
+			@include('layouts.ad', ['ad' => $metager->popAd()])
+		@endfor
+	@endif
+	{{-- Create results and ongoing ads --}}
+	@foreach($metager->getResults() as $result)
+		@if($mobile)
+			@if($result->number % 4 === 0)
+				@include('layouts.ad', ['ad' => $metager->popAd()])
+			@endif
+		@else
+			@if($result->number % 7 === 0)
+				@include('layouts.ad', ['ad' => $metager->popAd()])
+			@endif
+		@endif
+		@include('layouts.result', ['result' => $result])
+	@endforeach
+	@include('parts.pager')
+</div>
+@if( $metager->showQuicktips() )
+	<div id="quicktips"></div>
+@endif
