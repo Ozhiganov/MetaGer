47b3e6777f1dacee205bcb549f0ce14f206e4ea1
Aria <aria@suma-ev.de>
2018-03-05 10:08:06 +0100

    mix.js angepasst, translations,js wird nicht mehr in fast allen scripten eingefuegt

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,11 +18,11 @@ mix
   // js
   .scripts(['resources/assets/js/lib/jquery.js', 'resources/assets/js/lib/jquery-ui.min.js', 'resources/assets/js/lib/bootstrap.js', 'resources/assets/js/lib/lightslider.js',
     'resources/assets/js/lib/masonry.js', 'resources/assets/js/lib/imagesloaded.js', 'resources/assets/js/lib/openpgp.min.js', 'resources/assets/js/lib/iframeResizer.min.js',
-    'resources/assets/js/lib/md5.js', 'resources/assets/js/translations.js'], 'public/js/lib.js')
-  .scripts(['resources/assets/js/scriptStartPage.js', 'resources/assets/js/result-saver.js', 'resources/assets/js/translations.js'], 'public/js/scriptStartPage.js')
+    'resources/assets/js/lib/md5.js'], 'public/js/lib.js')
+  .scripts(['resources/assets/js/scriptStartPage.js', 'resources/assets/js/result-saver.js'], 'public/js/scriptStartPage.js')
   .scripts(['resources/assets/js/scriptResultPage.js', 'resources/assets/js/result-saver.js', 'resources/assets/js/translations.js'], 'public/js/scriptResultPage.js')
-  .scripts(['resources/assets/js/searchbar.js', 'resources/assets/js/translations.js'], 'public/js/searchbar.js')
-  .scripts(['resources/assets/js/focus-creator.js', 'resources/assets/js/translations.js'], 'public/js/focus-creator.js')
+  .scripts('resources/assets/js/searchbar.js', 'public/js/searchbar.js')
+  .scripts('resources/assets/js/focus-creator.js', 'public/js/focus-creator.js')
   // utility
   .scripts(['resources/assets/js/utility.js', 'resources/assets/js/translations.js'], 'public/js/utility.js')
   .version();
