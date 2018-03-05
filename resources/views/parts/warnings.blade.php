1a926f675008184dcf1b3ce51f8494cd94f9c742
Karl Hasselbring <Karl Hasselbring>
2018-03-05 09:59:36 +0100

    Die Blade Dateien der Ergebnisausgabe haben jetzt eine kürzere benennung Einige Dinge wurden Zusammengefasst und Redundanzen entfernt

---
+++ resources/views/parts/warnings.blade.php
@@ -0,0 +1,10 @@
+{{-- Show all warnings --}}
+@if(sizeof($warnings) > 0)
+	<div class="alert alert-warning">
+		<ul>
+			@foreach($warnings as $warning)
+				<li>{!! $warning !!}</li>
+			@endforeach
+		</ul>
+	</div>
+@endif
