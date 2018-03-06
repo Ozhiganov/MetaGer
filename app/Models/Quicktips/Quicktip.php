f04a638b8f5ea0ec51888fcec3882f5b8ca85d91
Karl Hasselbring <Karl Hasselbring>
2018-03-06 09:34:17 +0100

    Die Quicktips haben jetzt einen Verweis auf ihre Herkunft, sofern diese nicht MetaGer ist

--- app/Models/Quicktips/Quicktip.php
+++ app/Models/Quicktips/Quicktip.php
@@ -7,15 +7,19 @@ class Quicktip
     public $type;
     public $title;
     public $link;
+    public $gefVonTitle;
+    public $gefVonLink;
     public $descr;
     public $details;
 
     # Erstellt ein neues Ergebnis
-    public function __construct($type, $title, $link, $descr, $details)
+    public function __construct($type, $title, $link, $gefVonTitle, $gefVonLink, $descr, $details)
     {
         $this->type = $type;
         $this->title = $title;
         $this->link = $link;
+        $this->gefVonTitle = $gefVonTitle;
+        $this->gefVonLink = $gefVonLink;
         $this->descr = $descr;
         $this->details = $details;
     }
