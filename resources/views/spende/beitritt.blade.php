00cadd6dc36a5804a493e300eb7cc4f583f6b121
Aria <aria@suma-ev.de>
2018-04-27 11:21:06 +0200

    Seite sieht fast heile aus

--- resources/views/spende/beitritt.blade.php
+++ resources/views/spende/beitritt.blade.php
@@ -5,91 +5,90 @@
 @section('navbarFocus.donate', 'class="dropdown active"')
 
 @section('content')
-	<link type="text/css" rel="stylesheet" href="{{ mix('/css/beitritt.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ mix('/css/beitritt.css') }}" />	
+	<script src="{{ mix('js/scriptJoinPage.js') }}"></script>
+<form id="donation-form">
 	<div class="card-heavy">
 		<h1>{{ trans('beitritt.heading.1') }}</h1>
-		<form id="donation-form">
-			<div class="beitritt-inputs">
-				<div>
-					<label for="name">{{ trans('beitritt.beitritt.1') }}</label>
-					<input type="text" class="form-control beitritt-input" name="name" placeholder="{{trans('beitritt.placeholder.1')}}" required>
-				</div>
-				<div>
-					<label for="firma">{{ trans('beitritt.beitritt.2') }}</label>
-					<input type="text" class="form-control beitritt-input" name="firma" placeholder="{{trans('beitritt.placeholder.2')}}">
-				</div>
-				<div>
-					<label for="funktion">{{ trans('beitritt.beitritt.3') }}</label>
-					<input type="text" class="form-control beitritt-input" name="funktion" placeholder="{{trans('beitritt.placeholder.3')}}">
-				</div>
-				<div>
-					<label for="adresse">{{ trans('beitritt.beitritt.4') }}</label>
-					<input type="text" class="form-control beitritt-input" name="adresse" placeholder="{{trans('beitritt.placeholder.4')}}" required>
-				</div>
-				<div>
-					<label for="email">{{ trans('beitritt.beitritt.5') }}</label>
-					<input type="email" class="form-control beitritt-input" name="email" placeholder="">
-				</div>
-				<div>
-					<label for="homepage">{{ trans('beitritt.beitritt.6') }}</label>
-					<input type="text" class="form-control beitritt-input" name="homepage" placeholder="http://">
-				</div>
-				<div>
-					<label for="telefon">{{ trans('beitritt.beitritt.7') }}</label>
-					<input type="text" class="form-control beitritt-input" name="telefon" placeholder="{{trans('beitritt.placeholder.7')}}">
-				</div>
+		<div class="beitritt-inputs">
+			<div>
+				<label for="name">{{ trans('beitritt.beitritt.1') }}</label>
+				<input type="text" class="form-control beitritt-input" name="name" placeholder="{{trans('beitritt.placeholder.1')}}" required>
 			</div>
 			<div>
-				<p>{{ trans('beitritt.beitritt.8') }}</p>
-				<input type="text" class="form-control" name="betrag" required>
-				<p> {{ trans('beitritt.beitritt.9') }}</p>
+				<label for="firma">{{ trans('beitritt.beitritt.2') }}</label>
+				<input type="text" class="form-control beitritt-input" name="firma" placeholder="{{trans('beitritt.placeholder.2')}}">
 			</div>
 			<div>
-				<p>{{ trans('beitritt.beitritt.10') }}</p>
-				<p>{{ trans('beitritt.beitritt.11') }}</p>
+				<label for="funktion">{{ trans('beitritt.beitritt.3') }}</label>
+				<input type="text" class="form-control beitritt-input" name="funktion" placeholder="{{trans('beitritt.placeholder.3')}}">
 			</div>
 			<div>
-				<input type="radio" name="veröffentlichung" checked> {{ trans('beitritt.beitritt.12') }}
-				<input type="radio" name="veröffentlichung"> {{ trans('beitritt.beitritt.13') }}
+				<label for="adresse">{{ trans('beitritt.beitritt.4') }}</label>
+				<input type="text" class="form-control beitritt-input" name="adresse" placeholder="{{trans('beitritt.placeholder.4')}}" required>
 			</div>
 			<div>
-				<div>
-					<label for="ort">{{ trans('beitritt.beitritt.14') }}</label>
-					<input type="text" class="form-control beitritt-input" id="ort" placeholder="">
-				</div>
-				<p class="signature">{{ trans('beitritt.unterschrift') }}</p>
+				<label for="email">{{ trans('beitritt.beitritt.5') }}</label>
+				<input type="email" class="form-control beitritt-input" name="email" placeholder="">
+			</div>
+			<div>
+				<label for="homepage">{{ trans('beitritt.beitritt.6') }}</label>
+				<input type="text" class="form-control beitritt-input" name="homepage" placeholder="http://">
+			</div>
+			<div>
+				<label for="telefon">{{ trans('beitritt.beitritt.7') }}</label>
+				<input type="text" class="form-control beitritt-input" name="telefon" placeholder="{{trans('beitritt.placeholder.7')}}">
 			</div>
 		</div>
-		<div class="card-heavy">
-			<div class="newpage"></div>
-			<h1>{{ trans('beitritt.abbuchung.2') }}</h1>
-			<p>{{ trans('beitritt.abbuchung.3') }}</p>
-			<div class="beitritt-inputs">
-				<div>
-					<label for="kontoname">{{ trans('beitritt.abbuchung.4') }}</label>
-					<input type="text" class="form-control" name="kontoname" placeholder="">
-				</div>
-				<div>
-					<label for="bankverbindung">{{ trans('beitritt.abbuchung.5') }}</label>
-					<input type="text" class="form-control" name="bankverbindung" placeholder="">
-				</div>
-				<div>
-					<label for="iban">{{ trans('beitritt.abbuchung.6') }}</label>
-					<input type="text" class="form-control" name="iban" maxlength="22" placeholder="">
-				</div>
-				<div>
-					<label for="bic">{{ trans('beitritt.abbuchung.7') }}</label>
-					<input type="text" class="form-control" name="bic" placeholder="">
-				</div>
+		<div>
+			<p>{{ trans('beitritt.beitritt.8') }}</p>
+			<input type="text" class="form-control beitritt-input" name="betrag" required>
+			<p> {{ trans('beitritt.beitritt.9') }}</p>
+		</div>
+		<div>
+			<p>{{ trans('beitritt.beitritt.10') }}</p>
+			<p>{{ trans('beitritt.beitritt.11') }}</p>
+		</div>
+		<div>
+			<input type="radio" name="veröffentlichung" checked> {{ trans('beitritt.beitritt.12') }}
+			<input type="radio" name="veröffentlichung"> {{ trans('beitritt.beitritt.13') }}
+		</div>
+		<div>
+			<div>
+				<label for="ort">{{ trans('beitritt.beitritt.14') }}</label>
+				<input type="text" class="form-control beitritt-input" id="ort" placeholder="">
+			</div>
+			<p class="signature">{{ trans('beitritt.unterschrift') }}</p>
+		</div>
+	</div>
+	<div class="card-heavy">
+		<div class="newpage"></div>
+		<h1>{{ trans('beitritt.abbuchung.2') }}</h1>
+		<p>{{ trans('beitritt.abbuchung.3') }}</p>
+		<div class="beitritt-inputs">
+			<div>
+				<label for="kontoname">{{ trans('beitritt.abbuchung.4') }}</label>
+				<input type="text" class="form-control beitritt-input" name="kontoname" placeholder="">
 			</div>
 			<div>
-				<div>
-					<label for="ort2">{{ trans('beitritt.abbuchung.8') }}</label>
-					<input type="text" class="form-control beitritt-input" id="ort2" placeholder="">
-				</div>
-				<p class="signature">{{ trans('beitritt.unterschrift') }}</p>
+				<label for="bankverbindung">{{ trans('beitritt.abbuchung.5') }}</label>
+				<input type="text" class="form-control beitritt-input" name="bankverbindung" placeholder="">
 			</div>
-		</form>
+			<div>
+				<label for="iban">{{ trans('beitritt.abbuchung.6') }}</label>
+				<input type="text" class="form-control beitritt-input" name="iban" maxlength="22" placeholder="">
+			</div>
+			<div>
+				<label for="bic">{{ trans('beitritt.abbuchung.7') }}</label>
+				<input type="text" class="form-control beitritt-input" name="bic" placeholder="">
+			</div>
+		</div>
+		<div>
+			<label for="ort2">{{ trans('beitritt.abbuchung.8') }}</label>
+			<input type="text" class="form-control beitritt-input" id="ort2" placeholder="">
+			<p class="signature">{{ trans('beitritt.unterschrift') }}</p>
+		</div>
+		<hr>
 		<div class="beitritt-formular-info">
 			<p>{{ trans('beitritt.anweisung.1') }}</p>
 			<ul class="dotlist">
@@ -101,6 +100,6 @@
 			<p>{{ trans('beitritt.anweisung.6') }}</p>
 		</div>
 	</div>
+</form>
 	<button type="button" class="noprint js-only btn btn-lg btn-primary" onclick="window.print();">{{ trans('beitritt.anweisung.7') }}</button>
-	<!-- <script src="{{ mix('js/scriptJoinPage.js') }}"></script> -->
 @endsection
