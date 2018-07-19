1bd85dbe4f2cfc170352b8ab896cb8f5e395d3bb
Carsten Riel <carsten@suma-ev.de>
2018-07-19 11:16:07 +0200

    ID 'produktanzeigen' eingefügt

--- resources/lang/en/results.php
+++ resources/lang/en/results.php
@@ -6,4 +6,5 @@ return [
     "zurueck"        => "back",
     "filter"         => "Results are filtered by language: <strong>[:filter]</strong>. <a href=\":link\" target=\"_blank\">Disable this filter.</a>",
     "filter.default" => "The interface language is set to <strong>:langName</strong> but you will receive results in <strong>ALL</strong> languages. If you want to restrict the results to a specific language, please click  <a href=\"/en/settings\" target=\"_blank\">customize</a> and choose your desired language there.",
+    'produktanzeigen' => 'Show products:',
 ];
