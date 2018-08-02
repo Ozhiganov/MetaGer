54025e304d57945d1135bb4a1966f3b08c684e4c
Phil Höfer <phil@suma-ev.de>
2018-08-02 11:59:10 +0200

    Partnershop von der Startseite entfernt

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -4,9 +4,6 @@
     <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "kontakt") }}">{{ trans('sidebar.nav5') }}</a>
     <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "impressum") }}">{{ trans('sidebar.nav8') }}</a>
     <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "datenschutz") }}">{{ trans('sidebar.nav3') }}</a>
-    @if(LaravelLocalization::getCurrentLocale() == "de")
-      <a href="https://shop.spreadshirt.de/metager/" rel="noopener" target="_blank">{{ trans('sidebar.nav26') }}</a>
-    @endif
   </div>
   <div>
     <span class="hidden-xs">{{ trans('footer.sumaev.1') }} <a href="https://www.suma-ev.de/">{{ trans('footer.sumaev.2') }}</a></span>
