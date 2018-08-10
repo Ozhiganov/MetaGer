9833af8aabad1de80d8eeb356792e4b39f98acd1
Karl Hasselbring <karl@suma-ev.de>
2018-08-10 10:46:01 +0200

    New Style for the metager3 unstable warning

--- resources/lang/de/resultPage.php
+++ resources/lang/de/resultPage.php
@@ -3,6 +3,8 @@
 return [
     'opensearch' => 'MetaGer: Sicher suchen & finden, Privatsphäre schützen',
     'startseite' => 'MetaGer-Startseite',
-    'impressum'  => 'Impressum',
-    'search-placeholder' => 'Suchbegriff(e) eingeben'
+    'impressum' => 'Impressum',
+    'search-placeholder' => 'Suchbegriff(e) eingeben',
+    'metager3' => 'Sie befinden sich auf einer MetaGer Testversion. Hier werden Features vor der Veröffentlichung getestet. Unter Umständen funktioniert hier nicht alles wie gewohnt.
+    Unsere stabile Version finden Sie hier: <a href="https://metager.de" target="_self">metager.de</a>.',
 ];
