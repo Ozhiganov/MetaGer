aad0482e378a5131bee7f2ee05accae4b2b8839d
Dominik Hebeler <dominik@suma-ev.de>
2018-09-26 08:35:18 +0200

    First Version of our new Associator

--- resources/views/assoziator/asso.blade.php
+++ resources/views/assoziator/asso.blade.php
@@ -7,6 +7,20 @@
 		form div.input-group-addon button {
 			padding: 9px;
 		}
+
+		.reasso {
+			color: rgb(0, 102, 204);
+		}
+		.reasso:hover {
+			color: red;
+		}
+
+		td.association {
+			display: flex;
+		}
+		.asso-search-link {
+			margin-right: 10px;
+		}
 	</style>
 	<h1 class="page-title">{{ trans('asso.head.1') }}</h1>
 	<div class="card-heavy">
@@ -36,7 +50,12 @@
 					@foreach($words as $key => $value)
 					<tr>
 						<td>{{ $i }}</td>
-						<td>{{ $key }}</td>
+						<td class="association">
+							<a class="asso-search-link" href="{{ action('MetaGerSearch@search', ['eingabe' => $key]) }}" title="{{ trans('asso.searchasso.title') }}"><i class="fa fa-search" aria-hidden="true"></i></a>
+							<form method="POST">
+								<button name="q" value="{{ $key }}" class="reasso" type="submit" title="{{ trans('asso.reasso.title') }}">{{ $key }}</button>
+							</form>
+						</td>
 						<td>{{ round(($value / $wordCount) * 100, 2) }}%</td>
 						@php $i++; @endphp
 					</tr>
