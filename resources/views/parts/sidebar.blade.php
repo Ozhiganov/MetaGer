b2f4e875e2cfb950abbf26c0e4800f7bf6b92085
Karl Hasselbring <karl@suma-ev.de>
2018-10-02 09:23:35 +0200

    Sidebar Menüpunkte sind umsortiert

--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -4,20 +4,76 @@
     <p>MetaGer</p>
   </a>
   <ul class="sidebar-list">
-    <li @if ( !isset($navbarFocus) || $navbarFocus === 'suche') class="active" @endif >
+    <li>
       <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/") }}" tabindex="200" id="navigationSuche">
         <i class="fa fa-search" aria-hidden="true"></i>
-        <span>&nbsp;{{ trans('sidebar.nav1') }}</span>
+        <span>{{ trans('sidebar.nav1') }}</span>
       </a>
     </li>
-    <li @if (isset($navbarFocus) && $navbarFocus === 'dienste') class="metager-dropdown active" @else class="metager-dropdown" @endif >
+    <hr>
+    <li>
+      <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/datenschutz/") }}" id="navigationPrivacy" tabindex="215">
+        <i class="fa fa-user-secret" aria-hidden="true"></i>
+        <span>{{ trans('sidebar.nav3') }}</span>
+      </a>
+    </li>
+    <hr>
+    <li>
+      <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/hilfe/") }}" tabindex="217">
+        <i class="fa fa-info" aria-hidden="true"></i>
+        <span>{{ trans('sidebar.nav20') }}</span>
+      </a>
+    </li>
+    <li>
+      <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/faq/") }}" tabindex="218">
+        <i class="fa fa-question" aria-hidden="true"></i>
+        <span>{{ trans('sidebar.nav21') }}</span>
+      </a>
+    </li>
+    <hr>
+    <li>
+      <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/spende/") }}" tabindex="202">
+        <i class="fa fa-money" aria-hidden="true"></i>
+        <span>{{ trans('sidebar.nav2') }}</span>
+      </a>
+    </li>
+    <li>
+      <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/beitritt/") }}" tabindex="203">
+        <i class="fa fa-users" aria-hidden="true"></i>
+        <span>{{ trans('sidebar.nav23') }}</span>
+      </a>
+    </li>
+    <hr>
+    <li class="metager-dropdown">
+      <input id="contactToggle" class="sidebarCheckbox" type="checkbox">
+      <label for="contactToggle" class="metager-dropdown-toggle navigation-element" aria-haspopup="true" id="navigationKontakt" tabindex="219">
+        <i class="fa fa-comments-o" aria-hidden="true"></i>
+        <span>{{ trans('sidebar.nav18') }}</span>
+        <span class="caret"></span>
+      </label>
+      <ul class="metager-dropdown-content">
+        <li>
+          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/kontakt/") }}" tabindex="221">{{ trans('sidebar.nav5') }}</a>
+        </li>
+        <li>
+          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/team/") }}" tabindex="222">{{ trans('sidebar.nav6') }}</a>
+        </li>
+        <li>
+          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/about/") }}" tabindex="223">{{ trans('sidebar.nav7') }}</a>
+        </li>
+        <li>
+          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/impressum/") }}" tabindex="224">{{ trans('sidebar.nav8') }}</a>
+        </li>
+      </ul>
+    </li>
+    <li class="metager-dropdown">
       <input id="servicesToggle" class="sidebarCheckbox" type="checkbox">
         <label for="servicesToggle" class="metager-dropdown-toggle navigation-element" aria-haspopup="true" tabindex="225">
           <i class="fa fa-wrench" aria-hidden="true"></i>
-          <span>&nbsp;{{ trans('sidebar.nav15') }}</span>
+          <span>{{ trans('sidebar.nav15') }}</span>
           <span class="caret"></span>
         </label>
-      <ul class="metager-dropdown-menu">
+      <ul class="metager-dropdown-content">
         <li>
           <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/widget/") }}" tabindex="226">{{ trans('sidebar.nav10') }}</a>
         </li>
@@ -42,44 +98,6 @@
         <li>
           <a class="outlink" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/tor/") }}" tabindex="234">{{ trans('sidebar.nav14') }}</a>
         </li>
-      </ul>
-    </li>
-    <li @if (isset($navbarFocus) && $navbarFocus === 'datenschutz') class="active" @endif >
-      <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/datenschutz/") }}" id="navigationPrivacy" tabindex="215">
-        <i class="fa fa-user-secret" aria-hidden="true"></i>
-        <span>&nbsp;{{ trans('sidebar.nav3') }}</span>
-      </a>
-    </li>
-    <li @if (isset($navbarFocus) && $navbarFocus === 'hilfe') class="metager-dropdown active" @else class="metager-dropdown" @endif >
-      <input id="helpToggle" class="sidebarCheckbox" type="checkbox">
-      <label for="helpToggle" class="metager-dropdown-toggle navigation-element" aria-haspopup="true" id="navigationHilfe" tabindex="216">
-        <i class="fa fa-info-circle" aria-hidden="true"></i>
-        <span>&nbsp;{{ trans('sidebar.nav20') }}</span>
-        <span class="caret"></span>
-      </label>
-      <ul class="metager-dropdown-menu">
-        <li>
-          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/hilfe/") }}" tabindex="217">{{ trans('sidebar.nav20') }}</a>
-        </li>
-        <li>
-          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/faq/") }}" tabindex="218">{{ trans('sidebar.nav21') }}</a>
-        </li>
-      </ul>
-    </li>
-    <li @if (isset($navbarFocus) && $navbarFocus === 'foerdern') class="metager-dropdown active" @else class="metager-dropdown" @endif >
-      <input id="donationToggle" class="sidebarCheckbox" type="checkbox">
-      <label for="donationToggle" class="metager-dropdown-toggle navigation-element" tabindex="201">
-        <i class="fa fa-money" aria-hidden="true"></i>
-        <span>&nbsp;{{ trans('sidebar.nav16') }}</span>
-        <span class="caret"></span>
-      </label>
-      <ul class="metager-dropdown-menu">
-        <li>
-          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/spende/") }}" tabindex="202">{{ trans('sidebar.nav2') }}</a>
-        </li>
-        <li>
-          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/beitritt/") }}" tabindex="203">{{ trans('sidebar.nav23') }}</a>
-        </li>
         @if(LaravelLocalization::getCurrentLocale() == "de")
           <li>
             <a class="outlink" href="https://shop.spreadshirt.de/metager/" rel="noopener" target="_blank">{{ trans('sidebar.nav26') }}</a>
@@ -90,39 +108,14 @@
         </li>
       </ul>
     </li>
-    <li @if (isset($navbarFocus) && $navbarFocus === 'kontakt') class="metager-dropdown active" @else class="metager-dropdown" @endif >
-      <input id="contactToggle" class="sidebarCheckbox" type="checkbox">
-      <label for="contactToggle" class="metager-dropdown-toggle navigation-element" aria-haspopup="true" id="navigationKontakt" tabindex="219">
-        <i class="fa fa-comments-o" aria-hidden="true"></i>
-        <span>&nbsp;{{ trans('sidebar.nav18') }}</span>
-        <span class="caret"></span>
-      </label>
-      <ul class="metager-dropdown-menu">
-        <li>
-          <a class="outlink" href="http://forum.suma-ev.de/" tabindex="220">{{ trans('sidebar.nav4') }}</a>
-        </li>
-        <li>
-          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/kontakt/") }}" tabindex="221">{{ trans('sidebar.nav5') }}</a>
-        </li>
-        <li>
-          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/team/") }}" tabindex="222">{{ trans('sidebar.nav6') }}</a>
-        </li>
-        <li>
-          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/about/") }}" tabindex="223">{{ trans('sidebar.nav7') }}</a>
-        </li>
-        <li>
-          <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/impressum/") }}" tabindex="224">{{ trans('sidebar.nav8') }}</a>
-        </li>
-      </ul>
-    </li>
     <li class="metager-dropdown">
       <input id="languagesToggle" class="sidebarCheckbox" type="checkbox">
       <label for="languagesToggle" class="metager-dropdown-toggle navigation-element" aria-haspopup="true" id="navigationSprache" tabindex="235">
         <i class="fa fa-globe" aria-hidden="true"></i>
-        <span>&nbsp;{{ trans('sidebar.nav19') }} ({{ LaravelLocalization::getSupportedLocales()[LaravelLocalization::getCurrentLocale()]['native'] }})</span>
+        <span>{{ trans('sidebar.nav19') }} ({{ LaravelLocalization::getSupportedLocales()[LaravelLocalization::getCurrentLocale()]['native'] }})</span>
         <span class="caret"></span>
       </label>
-     <ul class="metager-dropdown-menu">
+      <ul class="metager-dropdown-content">
         @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
           <li>
             <a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}" tabindex="{{235 + $loop->index}}">{{{ $properties['native'] }}}</a>
