a08aebb9badb54077d4844f1b9a0c8a429d7323a
Dominik Hebeler <dominik@suma-ev.de>
2018-11-19 09:11:03 +0100

    Updated the manual for Firefox

--- resources/views/modals/plugin-modal.blade.php
+++ resources/views/modals/plugin-modal.blade.php
@@ -31,8 +31,8 @@
 					@if ($browser === 'Firefox' || $browser === 'Mozilla')
 						<h4>{!! trans('index.plugin.firefox.plugin') !!}</h4>
 						<ol>
-							<li>{{ trans('index.plugin.firefox.1') }}<img src="/img/Firefox.png" width="100%" /></li>
-							<li>{{ trans('index.plugin.firefox.2') }}<img src="/img/Firefox_Standard.png" width="100%" /></li>
+							<li>{!! trans('index.plugin.firefox.1') !!}<img src="/img/Firefox.png" width="100%" /></li>
+							<li>{!! trans('index.plugin.firefox.2') !!}<img src="/img/Firefox_Standard.png" width="100%" /></li>
 						</ol>
 						<hr />
 						<h4>{!! trans('index.plugin.firefox.3', ['browser' => $browser]) !!}</h4>
