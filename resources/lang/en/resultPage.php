579b28a3ca8b39992c29fac4e606b5d5e7b5bb6f
Karl Hasselbring <karl@suma-ev.de>
2018-11-19 09:17:40 +0100

    Changed few translations

--- resources/lang/en/resultPage.php
+++ resources/lang/en/resultPage.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    "opensearch"	=>	"MetaGer: secure search & find, protecting privacy",
-    "startseite"	=>	"MetaGer start page",
-    "impressum"	    =>	"legal notice",
-    'search-placeholder' => 'Your search query...'
+    "opensearch"         => "MetaGer: secure search & find, protecting privacy",
+    "startseite"         => "MetaGer start page",
+    "impressum"          => "site notice",
+    'search-placeholder' => 'Your search query...',
 ];
