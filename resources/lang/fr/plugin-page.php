552dc5de2240ac8a7ccc4c5a701030a807a36718
Karl Hasselbring <karl@suma-ev.de>
2018-11-26 12:26:37 +0100

    Übersetzungsdateien für Englisch und Deutsch sind vollständig

---
+++ resources/lang/fr/plugin-page.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "head.1"    => "Ajouter MetaGer à Firefox",
+    "head.2"    => "Ajouter MetaGer à Chrome",
+    "head.3"    => "Ajouter MetaGer à Opera",
+    "head.4"    => "Ajouter MetaGer à Internet Explorer",
+    "head.5"    => "Ajouter MetaGer à Microsoft Edge",
+    "head.6"    => "Ajouter MetaGer à Vivaldi",
+    "opera.10"  => "Cliquer sur \"ok\"",
+    "IE.11"     => "Cliquer sur \"ok\"",
+    "vivaldi.7" => "Cliquer sur \"ajouter\"",
+];
