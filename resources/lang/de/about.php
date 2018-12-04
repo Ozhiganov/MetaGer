e06d2df22a3882c89a08bf8453715348c0faee79
Carsten Riel <carsten@suma-ev.de>
2018-12-04 10:37:36 +0100

    Update resources/lang/de/about.php, resources/lang/de/spende.php files

--- resources/lang/de/about.php
+++ resources/lang/de/about.php
@@ -20,6 +20,6 @@ return [
     'list.3' => '<a href="https://de.wikipedia.org/wiki/MetaGer" target="_blank" rel="noopener">MetaGer</a> ist primär eine <a href="https://de.wikipedia.org/wiki/Metasuchmaschine" target="_blank" rel="noopener"><em>Meta</em>-Suchmaschine:</a> Wir fragen bis zu 50 Suchmaschinen ab. Damit können wir echte Vielfalt in den Ergebnissen liefern.',
     'list.4' => 'Wir bevorzugen in unseren Suchergebnissen nicht das, <a href="https://de.wikipedia.org/wiki/Filterblase" target="_blank" rel="noopener">was viel angeklickt wird</a>: Auch dadurch erhalten Sie nicht nur den Mainstream, sondern Vielfältigkeit.',
     'list.5' => '<a href="https://blog.suma-ev.de/node/207" target="_blank" rel="noopener">MetaGer ist seit 20 Jahren am Netz</a>, entstanden in der Uni-Hannover am <a href="http://noack-grasdorf.de" target="_blank" rel="noopener">RRZN</a>: Unsere Erfahrung ist Ihr Vorteil - wir wissen was wir tun.',
-    'list.6' => 'Unsere Server werden ausschließlich mit Ökostrom betrieben.',
+    'list.6' => 'Unsere Server werden ausschließlich mit nachhaltig produzierter Energie (Wasserkraft) betrieben.',
     'list.7' => 'Aber auch wir sind nicht fehlerfrei: Wenn Ihnen bei uns Merkwürdiges begegnet: Bitte <a href="/kontakt/" target="_blank" rel="noopener">kontaktieren Sie uns</a>! Wir nehmen Ihre Hinweise ernst: <em>Sie</em> sind uns das Wichtigste.',
 ];
