48e94a3bbd46e7db7e87ec341b45a6a121512a63
Carsten Riel <carsten@suma-ev.de>
2019-02-19 13:56:47 +0100

    Update resources/lang/de/faq.php, resources/lang/en/team.php, resources/lang/en/about.php, resources/lang/en/spende.php, resources/lang/de/spende.php, resources/lang/en/asso.php files

--- resources/lang/de/spende.php
+++ resources/lang/de/spende.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'headline.1' => 'Ihre Spende für SUMA-EV und MetaGer',
-    'headline.2' => 'Helfen Sie mit, dass freie Suchmaschinen im Internet frei bleiben. Das digitale Wissen der Welt muss ohne Bevormundung durch Staaten oder Konzerne frei zugänglich sein und bleiben. Für mehr Informationen über uns und wofür wir stehen klicken Sie bitte <a href="https://metager.de/about" rel="noopener" target=_blank>hier</a>.',
+    'headline.1'    => 'Ihre Spende für SUMA-EV und MetaGer',
+    'headline.2'    => 'Helfen Sie mit, dass freie Suchmaschinen im Internet frei bleiben. Das digitale Wissen der Welt muss ohne Bevormundung durch Staaten oder Konzerne frei zugänglich sein und bleiben. Für mehr Informationen über uns und wofür wir stehen klicken Sie bitte <a href="https://metager.de/about" rel="noopener" target=_blank>hier</a>.',
 
-    'bankinfo.1' => 'Spenden mittels einer Überweisung',
-    'bankinfo.2' => 'SUMA-EV
+    'bankinfo.1'    => 'Spenden mittels einer Überweisung',
+    'bankinfo.2'    => 'SUMA-EV
 IBAN: DE64 4306 0967 4075 0332 01
 BIC: GENODEM1GLS
 (Konto-Nr.: 4075 0332 01, BLZ: 43060967)
 GLS Gemeinschaftsbank, Bochum',
-    'bankinfo.3' => 'Falls Sie eine Spendenbescheinigung wünschen, teilen Sie uns bitte Ihre vollständige Adresse mit. Bei Spenden bis 200,-€ genügt der Kontoauszug für die Absetzbarkeit beim Finanzamt.',
+    'bankinfo.3'    => 'Falls Sie eine Spendenbescheinigung wünschen, teilen Sie uns bitte Ihre vollständige Adresse mit. Bei Spenden bis 200,-€ genügt der Kontoauszug für die Absetzbarkeit beim Finanzamt.',
 
     'lastschrift.1' => 'Spenden mittels elektronischem Lastschriftverfahren:',
     'lastschrift.2' => 'Tragen Sie hier Ihre Kontodaten sowie den gewünschten Betrag ein. Wir buchen dann entsprechend von Ihrem Konto ab. Notwendige Felder sind mit einem "*" gekennzeichnet.',
@@ -27,30 +27,30 @@ GLS Gemeinschaftsbank, Bochum',
     'lastschrift.9' => 'Spenden',
     'lastschrift.10' => 'Ihre Daten werden über eine verschlüsselte Verbindung zu uns übertragen und können von Dritten nicht mitgelesen werden. Der Betrag, den Sie angegeben haben, wird dann durch uns von Ihrem Konto abgebucht. SUMA-EV verwendet Ihre Daten ausschlie&szlig;lich für die Spendenabrechnung; Ihre Daten werden nicht weitergegeben. Spenden an den SUMA-EV sind steuerlich absetzbar, da der Verein vom Finanzamt Hannover Nord als gemeinnützig anerkannt ist, eingetragen in das Vereinsregister beim Amtsgericht Hannover unter VR200033.',
 
-    'drucken' => 'Drucken',
+    'drucken'   => 'Drucken',
 
-    'about.1' => 'Über uns',
-    'about.2' => 'Die deutsche Suchmaschine MetaGer wird betrieben und stetig weiterentwickelt vom SUMA-EV - Verein für freien Wissenszugang. Wir sind ein gemeinnützig anerkannter Verein, eines unserer Ziele ist die Entwicklung und der Betrieb von Suchmaschinen. Wir erhalten keine öffentlichen Fördergelder und sind daher auf Ihre Spenden angewiesen. Wir haben unsere Werbung stark reduziert, denn wir vertrauen auf IHRE Unterstützung.
+    'about.1'   => 'Über uns',
+    'about.2'   => 'Die deutsche Suchmaschine MetaGer wird betrieben und stetig weiterentwickelt vom SUMA-EV - Verein für freien Wissenszugang. Wir sind ein gemeinnützig anerkannter Verein, eines unserer Ziele ist die Entwicklung und der Betrieb von Suchmaschinen. Wir erhalten keine öffentlichen Fördergelder und sind daher auf Ihre Spenden angewiesen. Wir haben unsere Werbung stark reduziert, denn wir vertrauen auf IHRE Unterstützung.
 Helfen Sie mit, dass freie Suchmaschinen im Internet frei bleiben und stetig weiterentwickelt werden!
 Das können Sie mit einer Spende auf dieser Seite tun. Oder, wenn Sie freie Suchmaschinen auch langfristig sichern wollen:
             <a href="https://metager.de/beitritt" target="_blank" rel="noopener">Werden Sie Mitglied im Trägerverein von MetaGer, dem SUMA-EV.</a>',
-    'about.3' => 'Für Spenden können Sie unser <a href="https://metager.de/spende" rel="noopener" target="_blank">Spendenformular</a> nutzen. Oder werden Sie Mitglied (<a href="http://suma-ev.de/" target="_blank" rel="noopener">Zum Aufnahmeantrag</a>);Sie haben dann die Möglichkeit, auf den Seiten des SUMA-EV gegebenfalls Ihr Logo mit einem kurzen Text unterzubringen. Wie das aussieht, sehen Sie hier: <a href="https://suma-ev.de/suma-links/index.html#sponsors" target="_blank" rel="noopener">Sponsorenseite</a> und auch auf dieser Seite: <a href="https://suma-ev.de/mitglieder/index.html" target="_blank" rel="noopener">SUMA-EV Mitglieder</a>.<a href="https://metager.de/klassik/spenden1.html" target="_blank" rel="noopener">Oder werden Sie SUMA-EV Förderer!</a>',
-    'about.4' => '<a href="http://suma-ev.de/unterstuetzung/index.html" target="_blank" rel="noopener">JEDE Form Ihrer Unterstützung</a> hilft mit, dass freie Suchmaschinen und freier Wissenszugang im Internet eine Chance haben. Zum freien Wissenszugang gehört es auch, dass Ihre Daten weder überwacht, noch Ihre Internet-Adressen und Verbindungsdaten gespeichert werden. Bei uns wird Ihre Internet-Adresse bereits während der Suche anonymisiert, sie wird nicht gespeichert und nicht an Dritte weitergegeben. Freie Internet-Suche ohne Überwachung: <a href="https://metager.de/" target="_blank" rel="noopener">MetaGer.de!</a>',
+    'about.3' => 'Für Spenden können Sie unser <a href="https://metager.de/spende" rel="noopener" target="_blank">Spendenformular</a> nutzen. Oder werden Sie Mitglied (<a href="http://suma-ev.de/" target="_blank" rel="noopener">Zum Aufnahmeantrag</a>);Sie haben dann die Möglichkeit, auf den Seiten des SUMA-EV gegebenfalls Ihr Logo mit einem kurzen Text unterzubringen. Wie das aussieht, sehen Sie hier: <a href="https://suma-ev.de/suma-links/index.html#sponsors" target="_blank" rel="noopener">Sponsorenseite</a> und auch auf dieser Seite: <a href="https://suma-ev.de/mitglieder/index.html" target="_blank" rel="noopener">SUMA-EV Mitglieder</a>. Oder <a href="https://metager3.de/spende" target="_blank" rel="noopener">werden Sie SUMA-EV Förderer!</a>',
+    'about.4' => '<a href="https://suma-ev.de/unterstuetzung/index.html" target="_blank" rel="noopener">JEDE Form Ihrer Unterstützung</a> hilft mit, dass freie Suchmaschinen und freier Wissenszugang im Internet eine Chance haben. Zum freien Wissenszugang gehört es auch, dass Ihre Daten weder überwacht, noch Ihre Internet-Adressen und Verbindungsdaten gespeichert werden. Bei uns wird Ihre Internet-Adresse bereits während der Suche anonymisiert, sie wird nicht gespeichert und nicht an Dritte weitergegeben. Freie Internet-Suche ohne Überwachung: <a href="https://metager.de/" target="_blank" rel="noopener">MetaGer.de!</a>',
     'about.5' => 'Eine weitere Möglichkeit, MetaGer zu fördern, besteht darin, dass Sie Ihren nächsten Online-Einkauf bei MetaGer-Fördershops machen. Damit wir auf diesem Weg unterstützt werden können, haben wir uns in das Netzwerk zur Förderung gemeinnützig anerkannter Organisationen eingebracht, das Projekt <a href="https://www.boost-project.com/de" target="_blank" rel="noopener">www.boost-project.com</a> Unter dem Dach dieses Projektes sind ca. 400 Online-Shops (von 1und1 bis Zooplus) vereint, die sich bereit erklärt haben, von allen Verkäufen etwa 6% an das Projekt zu spenden. Statt wie bisher direkt zum Online-Shop zu surfen, gehen Sie zunächst auf <a href="https://metager.de/" target="_blank" rel="noopener">MetaGer.de!</a> und klicken dort oben in der Navigationsleiste auf "Fördern", dann auf <a href="https://www.boost-project.com/de/shops?charity_id=1129&amp;tag=bl" target="_blank" rel="noopener">"Einkaufen bei MetaGer Fördershops"</a>. Dieser Klick führt Sie in die Shop-Auswahl des Boost-Projektes. Dort suchen Sie sich Ihren Shop aus und machen wie gewohnt Ihren Einkauf. Oder klicken Sie für Einkäufe aus dem Bereich Schreibwaren, Büro- und Geschenkartikel direkt auf <a href="https://www.gladizon.com/" target="_blank" rel="noopener">www.gladizon.com</a>. Das ist alles. Wenn genug Menschen dies tun, dann brauchen wir keine Werbung mehr. Nur zwei Mausklicks für Sie - für alle eine Chance für den freien Wissenszugang in der digitalen Welt.',
 
-    'danke.title' => 'Herzlichen Dank!! Wir haben Ihre Spendenbenachrichtigung für MetaGer an SUMA-EV erhalten.',
-    'danke.nachricht' => 'Falls Sie Kontaktdaten angegeben haben, erhalten Sie demnächst auch eine persönliche Nachricht.',
-    'danke.kontrolle' => 'Folgende Nachricht hat uns erreicht:',
+    'danke.title'       => 'Herzlichen Dank!! Wir haben Ihre Spendenbenachrichtigung für MetaGer an SUMA-EV erhalten.',
+    'danke.nachricht'   => 'Falls Sie Kontaktdaten angegeben haben, erhalten Sie demnächst auch eine persönliche Nachricht.',
+    'danke.kontrolle'   => 'Folgende Nachricht hat uns erreicht:',
 
-    'telefonnummer' => 'Telefonnummer',
-    'iban' => 'IBAN/Kontonummer',
-    'bic' => 'BIC/Bankleitzahl',
-    'betrag' => 'Betrag',
-    'danke.message' => 'Ihre Nachricht',
+    'telefonnummer'     => 'Telefonnummer',
+    'iban'              => 'IBAN/Kontonummer',
+    'bic'               => 'BIC/Bankleitzahl',
+    'betrag'            => 'Betrag',
+    'danke.message'     => 'Ihre Nachricht',
 
-    'paypal.title' => 'Spenden mit Paypal',
-    'paypal.subtitle' => 'Dort auch per Kreditkarte ohne Registrierung',
-    'paypal.2' => 'Spenden Sie mit PayPal - schnell, kostenlos und sicher!',
+    'paypal.title'      => 'Spenden mit Paypal',
+    'paypal.subtitle'   => 'Dort auch per Kreditkarte ohne Registrierung',
+    'paypal.2'          => 'Spenden Sie mit PayPal - schnell, kostenlos und sicher!',
 
-    'bitpay.title' => 'Spenden mit Bitpay',
+    'bitpay.title'      => 'Spenden mit Bitpay',
 ];
