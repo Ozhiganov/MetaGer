63b9a5e74aa10d56a8a771db1d44aae010eec0c3
Dominik Hebeler <dominik@suma-ev.de>
2019-02-20 16:03:33 +0100

    Made unstable warning in english smaller too

--- resources/lang/en/resultPage.php
+++ resources/lang/en/resultPage.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    "opensearch"         => "MetaGer: secure search & find, protecting privacy",
-    "startseite"         => "MetaGer start page",
-    "impressum"          => "site notice",
+    "opensearch" => "MetaGer: secure search & find, protecting privacy",
+    "startseite" => "MetaGer start page",
+    "impressum" => "site notice",
     'search-placeholder' => 'Your search query...',
-    'metager3'           => 'You are currently on a MetaGer test version.<span class="hidden-xs"> It is used to try features before public release. Things might not work regularly. You can find our regular verion here: <a href="https://metager.de" target="_self">metager.de</a>.</span>',
+    'metager3' => 'You are currently on a MetaGer test version.',
 ];
