cd53bd17865cd96d97d68496426f57178acbe447
Dominik Hebeler <dominik@suma-ev.de>
2019-02-20 16:02:06 +0100

    Made unstable warning smaller

--- resources/lang/de/resultPage.php
+++ resources/lang/de/resultPage.php
@@ -5,6 +5,5 @@ return [
     'startseite' => 'MetaGer-Startseite',
     'impressum' => 'Impressum',
     'search-placeholder' => 'Suchbegriff(e) eingeben',
-    'metager3' => 'Sie befinden sich auf einer MetaGer Testversion. <span class="hidden-xs">Hier werden Features vor der Veröffentlichung getestet. Unter Umständen funktioniert hier nicht alles wie gewohnt.
-    Unsere stabile Version finden Sie hier: <a href="https://metager.de" target="_self">metager.de</a>.</span>',
+    'metager3' => 'Sie befinden sich auf einer MetaGer Testversion.',
 ];
