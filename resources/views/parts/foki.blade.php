be6150c1c211547333ee92d829c916073ab6c063
Dominik Hebeler <dominik@suma-ev.de>
2019-02-21 15:03:52 +0100

    readded a link to our maps

--- resources/views/parts/foki.blade.php
+++ resources/views/parts/foki.blade.php
@@ -3,3 +3,10 @@
 		<a href="@if($metager->getFokus() === $name)#@else{!!$metager->generateSearchLink($name)!!}@endif" target="_self" tabindex="0">@lang($fokus->{"display-name"})</a>
 	</div>
 @endforeach
+@if (LaravelLocalization::getCurrentLocale() == "de")
+<div id="maps">
+	<a href="https://maps.metager.de/map/{{ urlencode($metager->getQ()) }}/9.7380161,52.37119740000003,12" target="_blank">
+		Maps
+	</a>
+</div>
+@endif
