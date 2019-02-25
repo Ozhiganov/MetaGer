9834b42d00e636c47a36cb6332b1c71e0f01e777
Carsten Riel <carsten@suma-ev.de>
2019-02-25 10:01:53 +0100

    Update resources/lang/de/result.php, resources/lang/en/result.php files

--- resources/lang/en/result.php
+++ resources/lang/en/result.php
@@ -11,5 +11,6 @@ return [
     'options.6'         =>  'OPEN IN NEW TAB',
     'options.7'         =>  'OPEN',
     'options.more'      =>  'MORE',
+    'options.less'      =>  'LESS',
     'proxytext'	        =>	'Result link is opened anonymously. Your data will not be transfered to destination servers. Some links may not work as usual.'
 ];
