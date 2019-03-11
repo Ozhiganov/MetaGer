b0ddc1746f2522e34f602c20fd61869d11f05f2a
Dominik Hebeler <dominik@suma-ev.de>
2019-03-11 15:33:33 +0100

    added language files for alteration

--- resources/lang/de/results.php
+++ resources/lang/de/results.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-    'redirect'       => '(mit redirect)',
-    'weiter'         => 'Weitersuchen',
-    'zurueck'        => 'Zurück',
-    'filter'         => 'Die Suchergebnisse wurden gefiltert: <strong>[:filter]</strong>. <a href=":link" target="_blank">Filter abschalten.</a>',
+    'redirect' => '(mit redirect)',
+    'weiter' => 'Weitersuchen',
+    'zurueck' => 'Zurück',
+    'filter' => 'Die Suchergebnisse wurden gefiltert: <strong>[:filter]</strong>. <a href=":link" target="_blank">Filter abschalten.</a>',
     'filter.default' => 'Sie erhalten die Benutzeroberfläche in <strong>:langName</strong>, aber Ergebnisse in <strong>ALLEN</strong> Sprachen. Für ausschließlich deutschsprachige Ergebnisse können Sie unter <a href="/settings" target="_blank">anpassen</a> die "Sprache auswählen".',
 
     'produktanzeigen' => 'Produktanzeigen:',
+    'query.alteration' => 'Enthält Ergebnisse für <a href=":link">:alteration <i class="fa fa-search"></i></a>',
+    'query.alteration.original' => 'Stattdessen nach <a href=":link">:original <i class="fa fa-search"></i></a> suchen',
 ];
--- resources/views/parts/alteration.blade.php
+++ resources/views/parts/alteration.blade.php
@@ -1,7 +1,7 @@
 {{-- Show all infos --}}
 @if(!empty($metager->alteredQuery) && !empty($metager->alterationOverrideQuery))
 	<div class="alteration">
-		<div>Enthält Ergebnisse für <a href="{{ $metager->generateEingabeLink($metager->alteredQuery) }}">{{ $metager->alteredQuery }} <i class="fa fa-search"></i></a></div>
-		<div class="original">Stattdessen nach <a href="{{ $metager->generateEingabeLink($metager->alterationOverrideQuery) }}">{{$metager->getEingabe() }} <i class="fa fa-search"></i></a> suchen</div>
+		<div>@lang('results.query.alteration', ['link' => $metager->generateEingabeLink($metager->alteredQuery), 'alteration' => $metager->alteredQuery ])</div>
+		<div class="original">@lang('results.query.alteration.original', ['link' => $metager->generateEingabeLink($metager->alterationOverrideQuery), 'original' => $metager->getEingabe()])</div>
 	</div>
 @endif
