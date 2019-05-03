d8b656f8b7067ed7bf88da4e196f226b2b7564b2
Dominik Hebeler <dominik@suma-ev.de>
2019-05-03 13:57:13 +0200

    testing ci

--- test.sh
+++ test.sh
@@ -2,15 +2,4 @@
 
 set -e
 
-cd ~/MetaGer_neu
-
-# Start Webserver
-nohup php artisan serve --port 8005 >/dev/null 2>&1 &
-echo $! > ~/servePid
-
-# Start the Browserstack Proxy
-~/BrowserStackLocal --key gCyHqmJnNwLhp96LgiVG 2>&1 &
-echo $! > ~/browserstackPid
-sleep 5
-
 ./vendor/bin/phpunit
