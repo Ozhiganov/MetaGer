729e06f93fd4b48257e7f898c81a5fe92b61a67f
Dominik Hebeler <dominik@suma-ev.de>
2019-05-13 12:46:34 +0200

    changed log format

--- app/MetaGer.php
+++ app/MetaGer.php
@@ -1337,6 +1337,8 @@ class MetaGer
                 $logEntry .= " time=" . round((microtime(true) - $this->starttime), 2) . " serv=" . $this->fokus;
                 $logEntry .= " interface=" . LaravelLocalization::getCurrentLocale();
                 $logEntry .= " sprachfilter=" . $this->lang;
+                $logEntry .= " key=" . $this->apiKey;
+                $logEntry .= " eingabe=" . $this->eingabe;
 
                 # 2 Arten von Logs in einem wird die Anzahl der Abfragen an eine Suchmaschine gespeichert und in der anderen
                 # die Anzahl, wie häufig diese Ergebnisse geliefert hat.
