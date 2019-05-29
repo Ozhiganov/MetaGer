dbee982d921611589667bf4cd616cdd6768112c8
Dominik Hebeler <dominik@suma-ev.de>
2019-05-29 08:07:56 +0200

    fixed deployment for metager3

--- build.sh
+++ build.sh
@@ -26,3 +26,10 @@ npm install
 npm run production
 php artisan migrate --force
 php artisan db:seed --force
+php artisan requests:gather
+if [ -f ~/MetaGer/artisan ]; then php ~/MetaGer/artisan down;fi
+cd ~/
+while [ -d ~/MetaGer ]; do rm -rf ~/MetaGer;done
+mv MetaGer_neu MetaGer
+sudo pkill --signal SIGHUP supervisord
+php ~/MetaGer/artisan up
