48d692282a15e54f4528bc7a6b612deb301e5af1
Dominik Hebeler <dominik@suma-ev.de>
2019-06-06 12:32:19 +0200

    Added weekday to captcha

--- resources/views/humanverification/captcha.blade.php
+++ resources/views/humanverification/captcha.blade.php
@@ -3,21 +3,21 @@
 @section('title', $title )
 
 @section('content')
-    <h1>Entschuldigen Sie die Störung</h1>
-    <p>Sie befinden sich in einem Netzwerk aus dem wir verstärkt automatisierte Anfragen erhalten. Keine Sorge: Das bedeutet nicht unbedingt, dass diese Anfragen von Ihrem PC kommen.</p>
-    <p>Allerdings können wir Ihre Anfragen nicht von denen des "Robots" unterscheiden. Zum Schutz der von uns abgefragten Suchmaschinen müssen wir aber sicherstellen, dass diese nicht mit (automatisierten) Abfragen überflutet werden.</p>
-
-    <p>Bitte geben Sie deshalb die Zeichen aus dem Bild in die Eingabebox ein und bestätigen Sie mit "OK" um zur Ergebnisseite zu gelangen.</p>
-    <form method="post" action="{{ route('verification', ['id' => $id]) }}">
+    <h1>@lang('captcha.1')</h1>
+    <p>@lang('captcha.2')</p>
+    <p>@lang('captcha.3')</p>
+    <p>@lang('captcha.4')</p>
+    <form method="post" action="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), route('verification', ['id' => $id])) }}">
         <input type="hidden" name="url" value="{!! $url !!}">
         <input type="hidden" name="id" value="{{ $id }}">
         <p><img src="{{ $image }}" /></p>
         @if(isset($errorMessage))
         <p><font color="red">{{$errorMessage}}</font></p>
         @endif
-        <p><input type="text" class="form-control" name="captcha" placeholder="Captcha eingeben"  autofocus></p>
+        <p><input type="text" class="form-control" name="captcha" placeholder="@lang('captcha.5')" autofocus></p>
+        <p><input type="text" class="form-control" name="dw" placeholder="@lang('captcha.6')"></p>
         <p><button type="submit" class="btn btn-success" name="check">OK</button></p>
     </form>
-    <p>Sollten Sie diese Nachricht häufiger sehen oder handelt es sich dabei um einen Irrtum, schicken Sie uns gerne eine Nachricht über unser <a href="/kontakt">Kontaktformular</a>.</p>
-    <p>Nennen Sie uns in diesem Fall bitte unbedingt folgende Vorgangsnummer: {{ $id }}
+    <p>@lang('captcha.7', ['url' => LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), 'kontakt')])</p>
+    <p>@lang('captcha.8') {{ $id }}
 @endsection
--- routes/session.php
+++ routes/session.php
@@ -1,4 +1,11 @@
 <?php
 # In this File we collect all routes which require a session or other cookies to be active
-
-Route::match(['get', 'post'], 'meta/verification/{id}/{url?}', 'HumanVerification@captcha')->name('verification');
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(), /*,
+    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect' ]*/
+    ],
+    function () {
+        Route::match(['get', 'post'], 'meta/verification/{id}/{url?}', 'HumanVerification@captcha')->name('verification');
+    }
+);
