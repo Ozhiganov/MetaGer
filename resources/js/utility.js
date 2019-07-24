330ffb2c2b8f1311363e296f4bec02db9711c8ea
Aria <aria@suma-ev.de>
2019-07-24 11:25:29 +0200

    added possibility to tab through result-divs

--- resources/js/utility.js
+++ resources/js/utility.js
@@ -3,18 +3,39 @@ $(document).ready(function () {
   $('.no-js').addClass('hide');
 });
 
-document.onkeydown = checkKey;
+// Flag used for checking if the user was shown the keyboard control information
+ctrlInfo = false;
+currentResultIndex = 1;
 
+document.onkeydown = checkKey;
 function checkKey(e) {
 
     e = e || window.event;
 
-
     if (e.keyCode == '9') {
         e.preventDefault();
-        $("#keyboard-ctrl-info").show();
-        $("#keyboard-ctrl-info").focus();
-    } else {
+        guideThroughMenu($(document.activeElement));
     }
 
+}
+
+function guideThroughMenu(focused) {
+    if(!ctrlInfo) {
+        ctrlInfo = true;
+        showInfoBox();
+    } else {
+        focusNextResult();
+    }
+}
+
+function showInfoBox() {
+    $("#keyboard-ctrl-info").show();
+    $("#keyboard-ctrl-info").focus();
+}
+
+function focusNextResult() {
+    $("div[data-index='"+ currentResultIndex + "'").focus();
+    n = $(".result").length;
+    currentResultIndex = 1 + currentResultIndex % n;
+    console.log(currentResultIndex + " - " + n);
 }
