df0cae142fa8ac1ec15eb883c18eef692becaf28
Aria <aria@suma-ev.de>
2019-09-02 11:17:54 +0200

    Added hint for escape key usage

--- resources/views/layouts/keyboardNavBox.blade.php
+++ resources/views/layouts/keyboardNavBox.blade.php
@@ -7,7 +7,8 @@
                     <li><a href="#" id="results-nav" onclick="focusResults()" tabindex="1">Zu den Ergebnissen springen</a></li>
                     <li><a href="#" id="settings-nav" onclick="focusSettings()" tabindex="2">Zu den Sucheinstellungen springen</a></li>
                     <li><a href="#" id="site-nav" onclick="focusNavigation()" tabindex="3">Zu der Seitennavigation springen</a></li>
-                <ul>
+                </ul>
+                <p>Tipp: Drücken Sie die Escape-Taste um in diese Box zurückzugelangen und zum schließen aller ausklappbaren Menüs.</p>
             </div>
         </div>
     </div>
